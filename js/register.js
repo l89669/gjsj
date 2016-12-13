49e3bafe78bcdab84b72f9a0248eec69b3e0c7d0
mzhou <zhouwei9280@163.com>
2016-12-13 09:16:36 +0800

    提交注册验证

--- js/register.js
+++ js/register.js
@@ -1,30 +1,30 @@
 $('.register').click(function(){
 		var username = $('.username').val();
-		// if(username == ''){
-		// 	alert('游戏名不能为空');return false;
-		// }
+		if(username == ''){
+			alert('游戏名不能为空');return false;
+		}
 		var password = $('.password').val();
-		// if(password == ''){
-		// 	alert('密码不能为空');return false;
-		// }
+		if(password == ''){
+			alert('密码不能为空');return false;
+		}
 		var rpassword = $('.rpassword').val();
-		// if(password != rpassword){
-		// 	alert('密码两次输入的不同');return false;
-		// }
+		if(password != rpassword){
+			alert('密码两次输入的不同');return false;
+		}
 		var qq = $('.qq').val();
-		// if(qq == ''){
-		// 	alert('qq号不能为空');return false;
-		// }
+		if(qq == ''){
+			alert('qq号不能为空');return false;
+		}
 		var email = $('.email').val();
-		// if(email == ''){
-		// 	alert('邮箱不能为空');return false;
-		// }
-  // 		var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
-	 //    if(!myreg.test(email))
-  //       {
-  //           alert('请输入有效的E_mail！');
-  //           return false;
-  //     	}
+		if(email == ''){
+			alert('邮箱不能为空');return false;
+		}
+  		var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+	    if(!myreg.test(email))
+        {
+            alert('请输入有效的E_mail！');
+            return false;
+      	}
 		
 		var option = {
 			"username" : username,
